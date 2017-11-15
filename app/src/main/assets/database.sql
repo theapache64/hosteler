55fbddc458cd4be42a9d309ee6123f6935a55e62
theapache64-charlie <theapache64@gmail.com>
2017-11-15 21:16:52 +0530

    Building DB

--- app/src/main/assets/database.sql
+++ app/src/main/assets/database.sql
@@ -1,10 +1,13 @@
 DROP TABLE IF EXISTS food_history;
 CREATE TABLE food_history(
 	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	date_timestamp TEXT NOT NULL,
-	has_breakfast INTEGER NOT NULL CHECK(has_breakfast IN (0,1)),
-	has_dinner INTEGER NOT NULL CHECK(has_dinner IN (0,1)),
-	has_guest_breakfast INTEGER NOT NULL CHECK(has_breakfast IN (0,1)),
-    has_guest_dinner INTEGER NOT NULL CHECK(has_dinner IN (0,1)),
-	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+	_date TEXT NOT NULL,
+	breakfast INTEGER NOT NULL,
+	dinner INTEGER NOT NULL,
+	guest_breakfast INTEGER NOT NULL ,
+    guest_dinner INTEGER NOT NULL ,
+    additional_charge INTEGER NOT NULL,
+	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	UNIQUE (_date)
 );
+
