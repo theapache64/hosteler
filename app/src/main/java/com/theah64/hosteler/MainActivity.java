c6016aa742ba06eebbce326978a37a0af67f68ad
theapache64-alpha <theapache64@gmail.com>
2017-11-17 01:16:26 +0530

    .backup

--- app/src/main/java/com/theah64/hosteler/MainActivity.java
+++ app/src/main/java/com/theah64/hosteler/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends BaseAppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
