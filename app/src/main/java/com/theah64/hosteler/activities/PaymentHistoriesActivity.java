2c0ec6765af2a47530e82bcbdd43ab4168447ec5
theapache64-charlie <theapache64@gmail.com>
2017-11-20 19:28:02 +0530

    Added payment histories

--- app/src/main/java/com/theah64/hosteler/activities/PaymentHistoriesActivity.java
+++ app/src/main/java/com/theah64/hosteler/activities/PaymentHistoriesActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 
 import com.theah64.hosteler.R;
 import com.theah64.hosteler.adapters.PaymentHistoriesAdapter;
+import com.theah64.hosteler.database.tables.FoodHistories;
 import com.theah64.hosteler.database.tables.PaymentHistories;
 import com.theah64.hosteler.models.PaymentHistory;
 
@@ -21,6 +22,8 @@ public class PaymentHistoriesActivity extends BaseAppCompatActivity implements P
     public static final int RQ_CODE = 1;
     @BindView(R.id.rvPaymentHistories)
     RecyclerView rvPaymentHistories;
+    private List<PaymentHistory> paymentHistories;
+    private PaymentHistoriesAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +33,14 @@ public class PaymentHistoriesActivity extends BaseAppCompatActivity implements P
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        final List<PaymentHistory> paymentHistories = PaymentHistories.getInstance(this).getAll();
+        paymentHistories = PaymentHistories.getInstance(this).getAll();
         if (paymentHistories.isEmpty()) {
             Toast.makeText(this, "No history found", Toast.LENGTH_SHORT).show();
             finish();
         }
         rvPaymentHistories.setLayoutManager(new LinearLayoutManager(this));
-        rvPaymentHistories.setAdapter(new PaymentHistoriesAdapter(this, this, paymentHistories));
+        adapter = new PaymentHistoriesAdapter(this, this, paymentHistories);
+        rvPaymentHistories.setAdapter(adapter);
     }
 
     @Override
@@ -51,6 +55,13 @@ public class PaymentHistoriesActivity extends BaseAppCompatActivity implements P
 
     @Override
     public void onDeletePaymentHistory(int position) {
+        final PaymentHistory paymentHistory = paymentHistories.get(position);
 
+        //Removing connection
+        FoodHistories.getInstance(this).update(FoodHistories.COLUMN_PAYMENT_HISTORY_ID, paymentHistory.getId(), FoodHistories.COLUMN_PAYMENT_HISTORY_ID, null);
+        PaymentHistories.getInstance(this).delete(PaymentHistories.COLUMN_ID, paymentHistory.getId());
+
+        paymentHistories.remove(position);
+        adapter.notifyItemRemoved(position);
     }
 }
--- app/src/main/java/com/theah64/hosteler/database/tables/BaseTable.java
+++ app/src/main/java/com/theah64/hosteler/database/tables/BaseTable.java
@@ -8,7 +8,6 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
-
 import com.theah64.hosteler.utils.FileUtils;
 
 import java.io.IOException;
